e2d24dfbaf00f91c11eb0fd6f8c46eda80670f97
danielho-akl <danielho-akl@users.noreply.github.com>
2020-08-05 11:50:10 +1200

    Update B_python_script_split_std_80-20_WTCCC_T1D_weight_matrix.py

--- B_python_script_split_std_80-20_WTCCC_T1D_weight_matrix.py
+++ B_python_script_split_std_80-20_WTCCC_T1D_weight_matrix.py
@@ -1,5 +1,5 @@
 # Split the tissue specific eQTL matrix into two files (80% and 20%)
-# Denis_total_Gwas_cat_Denis_2017_all_combined_eQTL_table02042019.txt is the specific eQTL matrix with 4893 individual samples
+# Denis_total_Gwas_cat_Denis_2017_all_combined_eQTL_table02042019.txt is the individual tissue specific eQTL effect table matrix with 4893 individual samples (the Supplementary Table 5)
 
 
 import numpy as np
@@ -13,7 +13,7 @@ T1D_eQTL_table_tmp = T1D_eQTL_table.copy().reindex( np.random.permutation(T1D_eQ
 T1D_eQTL_table2 = T1D_eQTL_table_tmp.copy().reindex( np.random.permutation(T1D_eQTL_table_tmp.index))
 
 
- 
+# The split is appled to the total number of the individual genotype samples. Our Type 1 diabetes data have 4893 individuls. 80% of them is 3914 and 20% is 979. You may need to adjust the number according to the number of you genotype samples.  
 # Choose the first 3914 (out of 4893) examples for training.
 std_table80 = T1D_eQTL_table2.head(3914)
 
