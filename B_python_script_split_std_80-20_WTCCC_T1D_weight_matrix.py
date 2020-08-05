ec6b8b130981e2e4460700c35e2f0acc24b9006d
danielho-akl <danielho-akl@users.noreply.github.com>
2020-08-06 11:49:35 +1200

    Update B_python_script_split_std_80-20_WTCCC_T1D_weight_matrix.py

--- B_python_script_split_std_80-20_WTCCC_T1D_weight_matrix.py
+++ B_python_script_split_std_80-20_WTCCC_T1D_weight_matrix.py
@@ -1,11 +1,11 @@
 # Split the tissue specific eQTL matrix into two files (80% and 20%)
-# Denis_total_Gwas_cat_Denis_2017_all_combined_eQTL_table02042019.txt is the individual tissue specific eQTL effect table matrix with 4893 individual samples (the Supplementary Table 5)
+# Weighted_eQTL_matrix.txt is the individual tissue specific eQTL effect table matrix with 4893 individual samples (the Supplementary Table 5)
 
 
 import numpy as np
 import pandas as pd
 
-T1D_eQTL_table = pd.read_csv("data/Denis_total_Gwas_cat_Denis_2017_all_combined_eQTL_table02042019_v2.txt", sep="\t")
+T1D_eQTL_table = pd.read_csv("data/Weighted_eQTL_matrix.txt", sep="\t")
 
 
 T1D_eQTL_table_tmp = T1D_eQTL_table.copy().reindex( np.random.permutation(T1D_eQTL_table.index))
@@ -21,5 +21,5 @@ std_table80 = T1D_eQTL_table2.head(3914)
 # Choose the last 979 (out of 4893) examples for validation.
 std_table20 = T1D_eQTL_table2.tail(979)
 
-std_table80.to_csv('data/std80_Denis_total_Gwas_cat_Denis_2017_all_combined_eQTL_table02042019_v2.txt', sep='\t', index=False)
-std_table20.to_csv('data/std20_Denis_total_Gwas_cat_Denis_2017_all_combined_eQTL_table02042019_v2.txt', sep='\t', index=False)
+std_table80.to_csv('data/std80_Weighted_eQTL_matrix.txt', sep='\t', index=False)
+std_table20.to_csv('data/std20_Weighted_eQTL_matrix.txt', sep='\t', index=False)
